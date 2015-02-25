72da27e8fce6c590d79e63904a538365442b8def
Amit.Vyas <amitvyas@AmitVyass-Mac-mini.local>
2015-02-25 14:10:45 +0530

    first test commit

--- HelloWorld/AppDelegate.h
+++ HelloWorld/AppDelegate.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+//Comment to merely doing change in the file
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
