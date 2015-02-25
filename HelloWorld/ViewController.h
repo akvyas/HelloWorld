7a3f75dd773d5334ed7a2c9da6d5e6d5f7d0cd63
Amit.Vyas <amitvyas@AmitVyass-Mac-mini.local>
2015-02-25 14:04:54 +0530

    Initial Commit

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Amit.Vyas on 25/02/15.
+//  Copyright (c) 2015 Amit.Vyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Amit.Vyas on 25/02/15.
+//  Copyright (c) 2015 Amit.Vyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
